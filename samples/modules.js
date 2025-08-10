395c5c15bd347956353defa06844d596b7568e98
iamshonima <150287662+iamshonima@users.noreply.github.com>
2025-08-09 22:31:15 -0700

    Add modules import/export sample

---
+++ samples/modules.js
@@ -0,0 +1,10 @@
+// Modules in JavaScript
+
+// export
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// import example (would be in another file)
+// import { greet } from './modules';
+// console.log(greet('Alice'));
